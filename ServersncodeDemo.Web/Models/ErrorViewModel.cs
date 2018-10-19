67e7fd8ca111b46cb2fc62fd68823907489ddad8
James Kenny <james@extractorapp.com>
2018-10-19 10:26:51 +0100

    Add project files.

---
+++ ServersncodeDemo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServersncodeDemo.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
